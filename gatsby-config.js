c4d9c1933516d6270f4c5aa5d860e84db64f7cd5
unknown <jhnnytny@gmail.com>
2020-02-07 00:10:53 -0500

    don't set max width for remark images

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
         defaultLayouts: {
           default: require.resolve('./src/components/layout.js'),
         },
-        gatsbyRemarkPlugins: [{ resolve: 'gatsby-remark-images', options: { maxWidth: 450 } }],
+        gatsbyRemarkPlugins: [{ resolve: 'gatsby-remark-images' }],
         plugins: [{ resolve: 'gatsby-remark-images' }],
       },
     },
